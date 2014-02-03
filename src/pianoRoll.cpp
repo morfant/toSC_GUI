f86a9a04c0d420b277101f69ea2cfcb551a5d99a
Gangil <git>
2014-02-03 19:09:36 +0900

    Add PianoRoll class

---
+++ src/pianoRoll.cpp
@@ -0,0 +1,9 @@
+//
+//  pianoRoll.cpp
+//  toSC
+//
+//  Created by giy on 2/3/14.
+//
+//
+
+#include "pianoRoll.h"
---
+++ src/pianoRoll.h
@@ -0,0 +1,14 @@
+//
+//  pianoRoll.h
+//  toSC
+//
+//  Created by giy on 2/3/14.
+//
+//
+
+#ifndef __toSC__pianoRoll__
+#define __toSC__pianoRoll__
+
+#include <iostream>
+
+#endif /* defined(__toSC__pianoRoll__) */
